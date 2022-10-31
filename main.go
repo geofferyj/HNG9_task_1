ef5c053234e3eddfef432f9ecc31312e3dd15bf0
geofferyj <geofferyjoseph1@gmail.com>
2022-10-31 16:27:00 +0100

    modified:   main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 // return a json response
 func getHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, `{"slackUsername": "%s", "backend": "%t", "age": "%d", "bio": "%s"}`, "geofferyj", true, 27, "I am geofferyj, first of my name, the debugger of code, and all round bad guy.")
+	fmt.Fprintf(w, `{"slackUsername": "%s", "backend": %t, "age": %d, "bio": "%s"}`, "geofferyj", true, 27, "I am geofferyj, first of my name, the debugger of code, and all round bad guy.")
 }
 
 func main() {
